6adc2f43a3eedca84882090d0115c719b87dc494
Mark Lauter <mark.lauter@sumosoftware.com>
2018-12-12 14:11:50 -0500

    added geofence

---
+++ src/Sumo.Geo/GeoFences/GeoFence.cs
@@ -0,0 +1,23 @@
+﻿using Sumo.Geo.Geographies;
+using Sumo.Geo.Primitives;
+using System.Collections.Generic;
+
+namespace Sumo.Geo.GeoFences
+{
+    public partial class GeoFence
+    {
+        public List<Region> Regions { get; set; }
+
+        public bool Contains(GeoPoint point)
+        {
+            foreach (var region in Regions)
+            {
+                if (region.Contains(point))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ src/Sumo.Geo/GeoFences/GeoFenceIndex.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sumo.Geo.GeoFences
+{
+    class GeoFenceIndex
+    {
+    }
+}
