80f0a0983c8f7b39c41dea0221708dfde48ac29d
Mark Lauter <mark.lauter@sumosoftware.com>
2018-12-13 11:12:19 -0500

    renamed vector to displacement and moved to metrics namespace

---
+++ src/Sumo.Geo/Metrics/Displacement.cs
@@ -0,0 +1,26 @@
+﻿using Sumo.Geo.Primitives;
+using System;
+
+namespace Sumo.Geo.Metrics
+{
+    public partial class Displacement
+    {
+        public Displacement() { }
+
+        public Displacement(GeoPoint origin, Angle heading, Distance magnitude)
+        {
+            Origin = origin;
+            Heading = heading;
+            Distance = magnitude;
+        }
+
+        public Angle Heading { get; set; }
+        public Distance Distance { get; set; }
+        public GeoPoint Origin { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format($"{Origin}, {Heading}, {Distance}");
+        }
+    }
+}
--- src/Sumo.Geo/Metrics/Generated/Displacement.cs
+++ src/Sumo.Geo/Metrics/Generated/Displacement.cs
@@ -1,21 +1,21 @@
-﻿using Sumo.Geo.Metrics;
+﻿using Sumo.Geo.Primitives;
 using System;
 using System.Collections.Generic;
 
-namespace Sumo.Geo.Primitives
+namespace Sumo.Geo.Metrics
 {
-    public partial class Vector : IEquatable<Vector>
+    public partial class Displacement : IEquatable<Displacement>
     {
         public override bool Equals(object obj)
         {
-            return Equals(obj as Vector);
+            return Equals(obj as Displacement);
         }
 
-        public bool Equals(Vector other)
+        public bool Equals(Displacement other)
         {
             return other != null &&
                    EqualityComparer<Angle>.Default.Equals(Heading, other.Heading) &&
-                   EqualityComparer<Distance>.Default.Equals(Magnitude, other.Magnitude) &&
+                   EqualityComparer<Distance>.Default.Equals(Distance, other.Distance) &&
                    EqualityComparer<GeoPoint>.Default.Equals(Origin, other.Origin);
         }
 
@@ -23,17 +23,17 @@ namespace Sumo.Geo.Primitives
         {
             var hashCode = -594542511;
             hashCode = hashCode * -1521134295 + EqualityComparer<Angle>.Default.GetHashCode(Heading);
-            hashCode = hashCode * -1521134295 + EqualityComparer<Distance>.Default.GetHashCode(Magnitude);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Distance>.Default.GetHashCode(Distance);
             hashCode = hashCode * -1521134295 + EqualityComparer<GeoPoint>.Default.GetHashCode(Origin);
             return hashCode;
         }
 
-        public static bool operator ==(Vector vector1, Vector vector2)
+        public static bool operator ==(Displacement vector1, Displacement vector2)
         {
-            return EqualityComparer<Vector>.Default.Equals(vector1, vector2);
+            return EqualityComparer<Displacement>.Default.Equals(vector1, vector2);
         }
 
-        public static bool operator !=(Vector vector1, Vector vector2)
+        public static bool operator !=(Displacement vector1, Displacement vector2)
         {
             return !(vector1 == vector2);
         }
