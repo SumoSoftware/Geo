b4e16af1ad090ee2381563424744eeb208afb329
Mark Lauter <mark.lauter@sumosoftware.com>
2018-12-12 19:19:30 -0500

    implemented equality checks

---
+++ src/Sumo.Geo/Geographies/Generated/Circle.cs
@@ -0,0 +1,46 @@
+﻿using Sumo.Geo.Metrics;
+using Sumo.Geo.Primitives;
+using System;
+using System.Collections.Generic;
+
+namespace Sumo.Geo.Geographies
+{
+    public partial class Circle : Region, IEquatable<Circle>
+    {
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Circle);
+        }
+
+        public bool Equals(Circle other)
+        {
+            return other != null &&
+                   base.Equals(other) &&
+                   _radiusInNauticalMiles == other._radiusInNauticalMiles &&
+                   EqualityComparer<GeoPoint>.Default.Equals(Center, other.Center) &&
+                   EqualityComparer<Distance>.Default.Equals(_radius, other._radius) &&
+                   EqualityComparer<Distance>.Default.Equals(Radius, other.Radius);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1130478661;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + _radiusInNauticalMiles.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<GeoPoint>.Default.GetHashCode(Center);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Distance>.Default.GetHashCode(_radius);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Distance>.Default.GetHashCode(Radius);
+            return hashCode;
+        }
+
+        public static bool operator ==(Circle circle1, Circle circle2)
+        {
+            return EqualityComparer<Circle>.Default.Equals(circle1, circle2);
+        }
+
+        public static bool operator !=(Circle circle1, Circle circle2)
+        {
+            return !(circle1 == circle2);
+        }
+    }
+}
---
+++ src/Sumo.Geo/Geographies/Generated/Polygon.cs
@@ -0,0 +1,39 @@
+﻿using Sumo.Geo.Primitives;
+using System;
+using System.Collections.Generic;
+
+namespace Sumo.Geo.Geographies
+{
+    public partial class Polygon : Region, IEquatable<Polygon>
+    {
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Polygon);
+        }
+
+        public bool Equals(Polygon other)
+        {
+            return other != null &&
+                   base.Equals(other) &&
+                   EqualityComparer<GeoPath>.Default.Equals(Perimeter, other.Perimeter);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -510461622;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<GeoPath>.Default.GetHashCode(Perimeter);
+            return hashCode;
+        }
+
+        public static bool operator ==(Polygon polygon1, Polygon polygon2)
+        {
+            return EqualityComparer<Polygon>.Default.Equals(polygon1, polygon2);
+        }
+
+        public static bool operator !=(Polygon polygon1, Polygon polygon2)
+        {
+            return !(polygon1 == polygon2);
+        }
+    }
+}
