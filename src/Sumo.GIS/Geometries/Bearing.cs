0be43d096f9a1208fb4e26e7dcb9b2e8fa234cd6
Mark Lauter <mark.lauter@sumosoftware.com>
2018-12-20 13:11:36 -0500

    added class to represent Bearing

---
+++ src/Sumo.GIS/Geometries/Bearing.cs
@@ -0,0 +1,14 @@
+﻿using Sumo.GIS.Metrics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sumo.GIS.Geometries
+{
+    //todo: finish implementing bearing
+    public class Bearing
+    {
+        public CardinalDirections BaseLine { get; }
+        public Angle Heading { get; }
+    }
+}
