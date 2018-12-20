5077e4bbdb82648812f2cc0bfbcc21200e4ef8b2
Mark Lauter <mark.lauter@sumosoftware.com>
2018-12-20 00:04:50 -0500

    removed the Geographies namespace

--- src/Sumo.Geo/Geographies/TerrainMap.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sumo.Geo.Geographies
-{
-    /// <summary>
-    /// a raster of equidistant geo points with elevations that define a terrain surface
-    /// </summary>
-    public class TerrainMap : GeoPointCollection
-    {
-    }
-}
