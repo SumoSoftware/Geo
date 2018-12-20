f300417ad8f59f92d6613992eac3a65483d7ce84
Mark Lauter <mark.lauter@sumosoftware.com>
2018-12-20 14:21:25 -0500

    .

--- src/Sumo.GIS/Geometries/CoordinateReferenceSystems.cs
+++ src/Sumo.GIS/Geometries/CoordinateReferenceSystems.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Sumo.GIS.Geometries
+﻿namespace Sumo.GIS.Geometries
 {
     public enum CoordinateReferenceSystems
     {
