833e6dd9a703059e9f07cec31900d68b95d099ba
Mark Lauter <mark.lauter@sumosoftware.com>
2018-12-20 00:02:10 -0500

    added Features namespace

---
+++ src/Sumo.Geo/Features/IGeography.cs
@@ -0,0 +1,30 @@
+﻿//using Sumo.Geo.Metrics;
+
+//namespace Sumo.Geo.Geometries
+//{
+//    public interface IGeography
+//    {
+//        /// <summary>
+//        /// returns geodesic distance (great arc) between the entity and the provided GeoPoint
+//        /// </summary>
+//        /// <param name="geoEntity"></param>
+//        /// <returns></returns>
+//        Distance GetDistance(Point point, UnitsOfLength units = UnitsOfLength.NauticalMile);
+
+//        /// <summary>
+//        /// returns true if the entities is within the provided range provided of the provided GeoPoint
+//        /// </summary>
+//        /// <param name="geoEntity"></param>
+//        /// <param name="range"></param>
+//        /// <returns></returns>
+//        bool IsWithinRange(Point point, Distance range);
+
+//        /// <summary>
+//        /// returns the geo displacement to the provided entity
+//        /// geo displacement is the direction and distance from a source entity to a destination - basically a vector
+//        /// </summary>
+//        /// <param name="entity"></param>
+//        /// <returns></returns>
+//        Displacement GetDisplacement(Point point, UnitsOfLength units = UnitsOfLength.NauticalMile);
+//    }
+//}
