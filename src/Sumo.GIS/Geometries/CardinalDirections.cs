b17a6d78b1c0c6c87aa8bf3c4018ffce8524f59c
Mark Lauter <mark.lauter@sumosoftware.com>
2018-12-20 13:11:53 -0500

    added cardinal directions to support Bearing class

---
+++ src/Sumo.GIS/Geometries/CardinalDirections.cs
@@ -0,0 +1,14 @@
+﻿namespace Sumo.GIS.Geometries
+{
+    public enum CardinalDirections
+    {
+        North,
+        NorthEast,
+        East,
+        SouthEast,
+        South,
+        SouthWest,
+        West,
+        NorthWest
+    }
+}
