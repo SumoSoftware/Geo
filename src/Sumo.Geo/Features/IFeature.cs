9fc8fd5726ca6b916418155f8c16e8883d2bfefe
Mark Lauter <mark.lauter@sumosoftware.com>
2018-12-20 01:40:03 -0500

    added IFeature

---
+++ src/Sumo.Geo/Features/IFeature.cs
@@ -0,0 +1,11 @@
+﻿using Sumo.Geo.Geometries;
+using System.Collections.Generic;
+
+namespace Sumo.Geo.Features
+{
+    public interface IFeature
+    {
+        GeometryCollection Geometries { get; }
+        Dictionary<string, object> Properties { get; }
+    }
+}
