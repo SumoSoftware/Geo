ee4e0b7bbca5d4c7c0897669cf9efbb98598ae7b
Mark Lauter <mark.lauter@sumosoftware.com>
2018-12-19 22:40:21 -0500

    added Geometries namespace

---
+++ src/Sumo.Geo/Geometries/CoordinateReferenceSystems.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sumo.Geo.Geometries
+{
+    public enum CoordinateReferenceSystems
+    {
+        //https://tools.ietf.org/html/rfc7946#ref-WGS84
+        WGS84,
+
+    }
+}
---
+++ src/Sumo.Geo/Geometries/OrderedPoint.cs
@@ -0,0 +1,26 @@
+﻿using Sumo.Geo.Metrics;
+
+namespace Sumo.Geo.Geometries
+{
+    public sealed partial class OrderedPoint : Point
+    {
+        public OrderedPoint() : base() { }
+
+        public OrderedPoint(double latitude, double longitude, Distance elevation, int order) : base(latitude, longitude, elevation)
+        {
+            Order = order;
+        }
+
+        public OrderedPoint(Point point, int order) : base(point)
+        {
+            Order = order;
+        }
+
+        public int Order { get; set; }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}: {Order}";
+        }
+    }
+}
