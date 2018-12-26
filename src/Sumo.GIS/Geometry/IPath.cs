f97b13630fd2289bda2d0fd72c9667b337a90727
Mark Lauter <mark.lauter@sumosoftware.com>
2018-12-26 11:47:55 -0500

    added IPath

---
+++ src/Sumo.GIS/Geometry/IPath.cs
@@ -0,0 +1,11 @@
+﻿using Sumo.GIS.Metrics;
+
+namespace Sumo.GIS.Geometry
+{
+    public interface IPath
+    {
+        Point Origin { get; }
+        Point Terminus { get; }
+        Distance GetDistance(UnitsOfLength units);
+    }
+}
