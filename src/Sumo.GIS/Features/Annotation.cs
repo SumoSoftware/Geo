862e8ba1c094a623470f875a3b3105d585b30df5
Mark Lauter <mark.lauter@sumosoftware.com>
2018-12-26 12:58:31 -0500

    added Annotation feature

---
+++ src/Sumo.GIS/Features/Annotation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sumo.GIS.Features
+{
+    public class Annotation : IFeature
+    {
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Dictionary<string, object> Properties => throw new NotImplementedException();
+    }
+}
