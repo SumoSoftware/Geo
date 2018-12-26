adac21615475fc1bdc2418a6e0355e40895f623b
Mark Lauter <mark.lauter@sumosoftware.com>
2018-12-26 11:49:56 -0500

    Path now implements IPath

--- src/Sumo.GIS/Geometry/Path.cs
+++ src/Sumo.GIS/Geometry/Path.cs
@@ -6,7 +6,7 @@ namespace Sumo.GIS.Geometry
     /// <summary>
     /// linear feature  A geographic feature that can be represented by a line or set of lines.  For example, rivers, roads, and electric and telecommunication networks are all linear features.  
     /// </summary>
-    public class Path : PointCollection, IFigure
+    public class Path : PointCollection, IFigure, IPath
     {
         public Path()
         {
@@ -16,14 +16,19 @@ namespace Sumo.GIS.Geometry
         {
         }
 
-        public Distance GetDistance()
+        public Point Origin => Count > 0 ? this[0] : null;
+
+        public Point Terminus => Count > 0 ? this[Count - 1] : null;
+
+        public Distance GetDistance(UnitsOfLength units)
         {
+            // it's faster to keep the calculations in nautical miles until the sumation is complete because NM is the native units for distance calculations.
             var value = 0.0;
             for (var i = 0; i < Count - 2; ++i)
             {
                 value += this[i].GetDistance(this[i + 1], UnitsOfLength.NauticalMile).Value;
             }
-            return new Distance(value, UnitsOfLength.NauticalMile);
+            return new Distance(value, UnitsOfLength.NauticalMile).ConvertTo(units);
         }
     }
 }
