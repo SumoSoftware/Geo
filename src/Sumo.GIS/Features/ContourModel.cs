29f1f1e29b7dfff8be2b8fe84a13934b9324aed2
Mark Lauter <mark.lauter@sumosoftware.com>
2018-12-26 13:00:23 -0500

    fixed ContourModel and TerrainSurfaceModel

--- src/Sumo.GIS/Features/ContourModel.cs
+++ src/Sumo.GIS/Features/ContourModel.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 
 namespace Sumo.GIS.Features
 {
-    public class ContourModel : IModel
+    public class ContourModel : FigureCollection<ContourLine>, IFeature, IModel<ContourLine>
     {
         public string Name { get; set; }
         public Dictionary<string, object> Properties { get; } = new Dictionary<string, object>();
 
-        public List<ContourLine> Lines { get; } = new List<ContourLine>();
+        public FigureCollection<ContourLine> Figures => this;
+
+        //todo: implement GetExtent
+        public override Rectangle GetExtent()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- src/Sumo.GIS/Features/TerrainSurfaceModel.cs
+++ src/Sumo.GIS/Features/TerrainSurfaceModel.cs
@@ -7,10 +7,12 @@ namespace Sumo.GIS.Features
     /// digital elevation model(DEM)  1.  A digital representation of a continuous variable over a two- dimensional surface by a regular array of z-values referenced to a common datum.Digital elevation models are typically used to represent terrain relief.Also referred to as 'digital terrain model' (DTM).  2.  An elevation database for elevation data by map sheet from the National Mapping Division of the U.S.Geological Survey (USGS).  3.  The format of the USGS digital elevation data sets.USGS DEMs are produced by the Survey Branch of the United States Department of the Interior, consisting of a regular array of elevations referenced in the Universal Transverse Mercator (UTM) coordinate system.These data correspond to the standard 1:24,000-scale 7.5 x 7.5- minute quadrangles or 1:250,000 one-degree map sheets.Elevations are in meters or feet referenced to mean sea level.  
     /// a raster of equidistant geo points with elevations that define a terrain surface
     /// </summary>
-    public class TerrainSurfaceModel : PointCollection, IModel
+    public class TerrainSurfaceModel : PointCollection, IFeature, IModel<Point>
     {
         public string Name { get; set; }
         public Dictionary<string, object> Properties { get; } = new Dictionary<string, object>();
+
+        public FigureCollection<Point> Figures => this;
     }
 }
 
